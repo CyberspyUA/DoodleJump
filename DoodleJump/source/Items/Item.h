55c1e9224dc3153812d38b7c4633d30fb8ad88c2
Nikita Struk <88832410+CyberspyUA@users.noreply.github.com>
2024-01-21 22:34:00 +0200

    Major updates

--- DoodleJump/source/Items/Item.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "source/Entity.h"
-
-class Item : public Entity
-{
-public:
-	using Entity::Entity;
-};
-
--- DoodleJump/source/Platforms/Platform.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "../Entity.h"
-
-class Platform : public Entity {
-public:
-    using Entity::Entity;
-};
